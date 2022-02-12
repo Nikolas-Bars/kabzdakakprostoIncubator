38cec2b3150047a59204f1a2ffbb1bb8c864135f
Nikolas-Bars <kasp2409@mail.ru>
2022-02-12 15:11:34 +0300

    a

--- src/Components/Rating/Rating.tsx
+++ src/Components/Rating/Rating.tsx
@@ -9,53 +9,80 @@ function Rating(props: RatingPropsType) {
 
     let [raiting, setRaiting] = useState(0)
 
-    const buttonRaiting1 =()=>{
+    const buttonRaiting1 = () => {
         setRaiting(1)
     }
 
-    const buttonRaiting2 =()=>{
+    const buttonRaiting2 = () => {
         setRaiting(2)
     }
-    const buttonRaiting3 =()=>{
+    const buttonRaiting3 = () => {
         setRaiting(3)
     }
-    const buttonRaiting4 =()=>{
+    const buttonRaiting4 = () => {
         setRaiting(4)
     }
-    const buttonRaiting5 =()=>{
+    const buttonRaiting5 = () => {
         setRaiting(5)
     }
 
 
+    return (
+        <div>
+            <button onClick={buttonRaiting1}>1</button>
+            <button onClick={buttonRaiting2}>2</button>
+            <button onClick={buttonRaiting3}>3</button>
+            <button onClick={buttonRaiting4}>4</button>
+            <button onClick={buttonRaiting5}>5</button>
 
- return (
-     <div>
-         <button onClick={buttonRaiting1}>1</button>
-         <button onClick={buttonRaiting2}>2</button>
-         <button onClick={buttonRaiting3}>3</button>
-         <button onClick={buttonRaiting4}>4</button>
-         <button onClick={buttonRaiting5}>5</button>
 
-
-         <Star selected={raiting > 0}/>
-         <Star selected={raiting > 1}/>
-         <Star selected={raiting > 2}/>
-         <Star selected={raiting > 3}/>
-         <Star selected={raiting > 4}/>
-     </div>
- )
+            <Star raiting={raiting} button={buttonRaiting1} selected={raiting > 0}/>
+            <Star raiting={raiting} button={buttonRaiting2} selected={raiting > 1}/>
+            <Star raiting={raiting} button={buttonRaiting3} selected={raiting > 2}/>
+            <Star raiting={raiting} button={buttonRaiting4} selected={raiting > 3}/>
+            <Star raiting={raiting} button={buttonRaiting5} selected={raiting > 4}/>
+        </div>
+    )
 }
 
 type StarPropsType = {
     selected: boolean
+    button: () => void
+    raiting: number
 }
 
+
 function Star(props: StarPropsType) {
+
+    let divStyle = {
+        display: 'inline-block',
+        width: '30px',
+        height: '30px',
+        border: '1px solid gray',
+        padding: '5px',
+        margin: '5px',
+        alignContent: 'center',
+        borderRadius: '15px',
+        backgroundColor: 'gold'
+    }
+
+    let divStyle1 = {
+        display: 'inline-block',
+        width: '30px',
+        height: '30px',
+        border: '1px solid gray',
+        padding: '5px',
+        margin: '5px',
+        alignContent: 'center',
+        borderRadius: '15px',
+        backgroundColor: 'gray'
+    }
+
     console.log("Star is rendering")
-    if(props.selected){
-       return <span><b>Star </b></span>
-    }else{
-       return <span>Star </span>
+    if (props.selected) {
+        return <div style={divStyle} onClick={props.button}><span ><b>Star </b></span></div>
+    } else {
+        return <div style={divStyle1} onClick={props.button}><span>Star </span></div>
     }
 }
 
